32d880d15b3c29574be9a7176f4f52dd03a86dec
Nidheesh J <nidheesh.jagadeesan@phases.dk>
2018-09-19 11:29:05 +0530

    created

---
+++ createRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+class createRepository
+{
+    public $repository;
+
+    public function __construct($arg)
+    {
+        $this->repository = $arg;
+    }
+
+    public function create()
+    {
+        $dir = "app/Repository/" . $this->repository;
+        if (!file_exists($dir) && !is_dir($dir)) {
+            mkdir($dir, 0777, true);
+        }
+        $interfaceDir = $dir . "/" . $this->repository . "Interface.php";
+        $interfaceContent = "<?php
+namespace " . $this->repository . "\Repositories\\" . $this->repository . ";
+
+interface " . $this->repository . "Interface
+{
+
+}
+";
+        $repositoryDir = $dir . "/" . $this->repository . "Repository.php";
+        $repositoryContent = "<?php
+namespace " . $this->repository . "\Repositories\\" . $this->repository . ";
+
+class " . $this->repository . "Repository implements " . $this->repository . "Interface
+{
+
+}
+";
+        try {
+            if (file_put_contents($interfaceDir, $interfaceContent) && file_put_contents($repositoryDir, $repositoryContent)) {
+                echo 'Repository successfully created
+';
+            }
+        } catch (Exception $exception){
+            echo $exception;
+        }
+
+
+    }
+}
+
+$repo = new createRepository($argv[1]);
+
+$repo->create();
